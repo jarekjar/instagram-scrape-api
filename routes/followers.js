ea5b523bc159908e7c45d2f65dbdd4a2431f47a9
jared <jaredkjar@gmail.com>
2021-05-26 15:10:25 -0700

    added notes

--- routes/followers.js
+++ routes/followers.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 const puppeteer = require("puppeteer");
 
+//global variables to track progress/data
+//TODO: bind these to a session/cookie so that more than one scraper can run at a time
 let currentLength = 0;
 let failed = false;
 let finished = false;
@@ -9,6 +11,7 @@ let followerData = [];
 let totalFollowers = 0;
 let loading = false;
 
+//clear global var 
 const clearData = () => {
   currentLength = 0;
   failed = false;
@@ -18,22 +21,24 @@ const clearData = () => {
   loading = false;
 }
 
-/* GET followers */
+/* GET followers main route /followers */
 router.get("/", async (req, res, next) => {
 
+  //it a scrape is in progress return a 500
   if(currentLength !== 0 && !failed) {
     res.status(500).send("Scraping in progress.");
     return;
   }
 
+  //if last scrape failed, clear the data and continue
   if(currentLength !== 0 && failed){
     clearData();
   }
 
-  const username = req.query.username;
-  const password = req.query.password;
-  const profile = req.query.profile;
+  //destructure and assign the query parameters
+  const { username, password, profile } = req.query;
 
+  //validate the query paramters, return error if failed
   if (!username || !password || !profile) {
     res
       .status(400)
@@ -45,6 +50,7 @@ router.get("/", async (req, res, next) => {
     return;
   }
 
+  //route main try
   try {
     const browser = await puppeteer.launch({ headless: false });
 
@@ -52,6 +58,9 @@ router.get("/", async (req, res, next) => {
 
     await page.goto("https://www.instagram.com");
 
+
+    //this is the main event listener, that checks for "query_hash" in the api calls 
+    //while scrolling and collects and appends the data to the global variable
     page.on("response", async (response) => {
       try{
         if (response.url().includes("query_hash")) {
@@ -64,16 +73,20 @@ router.get("/", async (req, res, next) => {
           }
           if (
             responseData &&
-            responseData.user &&
+            responseData.data &&
             responseData.data.user &&
             responseData.data.user.edge_followed_by
           ) {
+            //append new data with old data
             followerData = [...followerData, ...responseData.data.user.edge_followed_by.edges];
-            loading = false;
             console.log(followerData.length);
             currentLength = followerData.length;
+            //tell the scraper to scroll again
+            scroll();
           }
         }
+
+        //this one checks for a failed login call, and return invalid IG message
         if (response.url().includes("ajax")) {
           const responseData = await response.json();
           if (responseData.status === "fail") {
@@ -98,43 +111,51 @@ router.get("/", async (req, res, next) => {
       }
     });
 
+
+    //find the login elements and input the user details to log in
     let userInput = await page.waitForSelector('input[name="username"]');
     let passwordInput = await page.waitForSelector('input[name="password"]');
     let logIn = await page.waitForSelector('button[type="submit"]');
-
     await userInput.type(username);
     await passwordInput.type(password);
     await logIn.click();
     await page.waitForSelector('img[data-testid="user-avatar"]');
     await page.goto(`https://www.instagram.com/${profile}`);
 
+    //find the followers button
     let followersButton = await page.waitForSelector(
       `a[href="/${profile}/followers/"]`
     );
 
+    //find and set the followers count
     totalFollowers = await page.evaluate((profile) => {
       return parseInt(document.querySelector(`a[href="/${profile}/followers/"] > span`).textContent.replace(/,/g, ''), 10)
     }, profile);
 
+
+    //here is where you can set a hard limit for the scraper
+    totalFollowers = 300;
+
+    //return status OK with current progres, and begin the scraping
+    //client is expected to call /followers/status to see the current status
     res.status(200).send({ inProgress: true, progress: currentLength, totalFollowers: totalFollowers });
 
+    
     await followersButton.click();
 
     await page.waitForSelector("div[role='dialog'] > div > div:nth-child(2)");
 
+    //asychronous scroll call
+    //this tells the followers div to keep scrolling 
+    //and is called every time new data comes in
     const scroll = async () => {
       try{
-        !loading && page.evaluate(() => document.querySelector("div[role='dialog'] > div > div:nth-child(2)").scrollTop += 1200);
-        loading = true;
-        await new Promise(resolve => setTimeout(resolve, 400));
-        if(followerData.length >= 1000){
+        page.evaluate(() => document.querySelector("div[role='dialog'] > div > div:nth-child(2)").scrollTop += 1200);
+        if(followerData.length >= totalFollowers){
           finished = true;
           loading = false;
           await browser.close();
           return;
-        }
-        else {
-          scroll();
         }
       }
      catch(err) {
@@ -143,7 +164,13 @@ router.get("/", async (req, res, next) => {
      }
     };
 
-    await scroll();
+    //call the defined scroll call
+    await scroll().catch(err => {
+      failed = true;
+      console.log(err);
+      browser.close();
+      return;
+    });
 
   } catch (err) {
     console.log(err);
@@ -153,16 +180,19 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-/* GET home page. */
+/* GET status. */
 router.get('/status', function(req, res, next) {
+  //if the job has finished, return data to the client
   if(finished){
     res.status(200).send(followerData);
     clearData();
   }
+  //if the job failed, return error to the client
   else if(failed){
     res.status(500).send("Scraper has failed.");
     clearData();
   }
+  //return current scraper job status
   else res.status(202).send({ progress: currentLength, totalFollowers: totalFollowers });
 });
 
