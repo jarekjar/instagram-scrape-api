02bf4035ddb4f81018e18eabb27dbc1be3a19c71
jared <jaredkjar@gmail.com>
2021-05-26 14:23:32 -0700

    added status call and progress indication

--- routes/followers.js
+++ routes/followers.js
@@ -2,9 +2,33 @@ var express = require("express");
 var router = express.Router();
 const puppeteer = require("puppeteer");
 
+let currentLength = 0;
+let failed = false;
+let finished = false;
+let followerData = [];
+let totalFollowers = 0;
+let loading = false;
+
+const clearData = () => {
+  currentLength = 0;
+  failed = false;
+  finished = false;
+  followerData = [];
+  totalFollowers = 0;
+  loading = false;
+}
+
 /* GET followers */
 router.get("/", async (req, res, next) => {
-  let followerData = [];
+
+  if(currentLength !== 0 && !failed) {
+    res.status(500).send("Scraping in progress.");
+    return;
+  }
+
+  if(currentLength !== 0 && failed){
+    clearData();
+  }
 
   const username = req.query.username;
   const password = req.query.password;
@@ -31,36 +55,44 @@ router.get("/", async (req, res, next) => {
     page.on("response", async (response) => {
       try{
         if (response.url().includes("query_hash")) {
-          const responseData = await response.json();
+          let responseData;
+          try {
+            responseData = await response.json();
+          }
+          catch(err){
+            return;
+          }
           if (
             responseData &&
+            responseData.user &&
             responseData.data.user &&
             responseData.data.user.edge_followed_by
           ) {
-            followerData.concat(responseData.data.user.edge_followed_by.edges);
-            //res.status(200).send(responseData.data.user.edge_followed_by.edges);
-            //await browser.close();
-            //return;
+            followerData = [...followerData, ...responseData.data.user.edge_followed_by.edges];
+            loading = false;
             console.log(followerData.length);
+            currentLength = followerData.length;
           }
         }
         if (response.url().includes("ajax")) {
           const responseData = await response.json();
           if (responseData.status === "fail") {
             res.status(400).send(responseData.message);
+            failed = true;
             await browser.close();
             return;
           }
           if (responseData.authenticated === false) {
             res.status(400).send("Invalid instagram login.");
+            failed = true;
             await browser.close();
             return;
           }
         }
       }
       catch(err) {
-        //res.status(500).send("Error occured.");
         console.log(err);
+        failed = true;
         await browser.close();
         return;
       }
@@ -80,19 +112,24 @@ router.get("/", async (req, res, next) => {
       `a[href="/${profile}/followers/"]`
     );
 
+    totalFollowers = await page.evaluate((profile) => {
+      return parseInt(document.querySelector(`a[href="/${profile}/followers/"] > span`).textContent.replace(/,/g, ''), 10)
+    }, profile);
+
+    res.status(200).send({ inProgress: true, progress: currentLength, totalFollowers: totalFollowers });
+
     await followersButton.click();
 
     await page.waitForSelector("div[role='dialog'] > div > div:nth-child(2)");
 
-    //const followersDiv = page.evaluate(() => document.querySelector("div[role='dialog'] > div > div:nth-child(2)"));
-
     const scroll = async () => {
       try{
-        page.evaluate(() => document.querySelector("div[role='dialog'] > div > div:nth-child(2)").scrollTop += 1200);
+        !loading && page.evaluate(() => document.querySelector("div[role='dialog'] > div > div:nth-child(2)").scrollTop += 1200);
+        loading = true;
         await new Promise(resolve => setTimeout(resolve, 400));
-        scroll();
-        if(followerData.length >= 200){
-          res.status(200).send(followerData);
+        if(followerData.length >= 1000){
+          finished = true;
+          loading = false;
           await browser.close();
           return;
         }
@@ -102,16 +139,31 @@ router.get("/", async (req, res, next) => {
       }
      catch(err) {
        console.log(err);
+       failed = true;
      }
     };
 
-    return await scroll();
+    await scroll();
 
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).send("Scraper could not connect.");
+    failed = true;
     return;
   }
+});
+
+/* GET home page. */
+router.get('/status', function(req, res, next) {
+  if(finished){
+    res.status(200).send(followerData);
+    clearData();
+  }
+  else if(failed){
+    res.status(500).send("Scraper has failed.");
+    clearData();
+  }
+  else res.status(202).send({ progress: currentLength, totalFollowers: totalFollowers });
 });
 
 module.exports = router;
